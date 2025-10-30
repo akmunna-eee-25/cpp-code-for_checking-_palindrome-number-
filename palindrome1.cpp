384653a8813c7f159c580faf3ba7be870ff2903b
Ashraful kabir Munna <kabirmunna98@gmail.com>
2025-10-30 20:33:56 +0600

    palindrome checking code
    by using cpp code you can check any number palindrome or not

---
+++ palindrome1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{   while(1)
+
+{
+
+     long long rev=0,result,n;
+     cout<<" Enter the value for checking palindrome:";
+     cin>>n;
+     long long a=n;
+     while(n>0){
+
+        result=n%10;
+        n=n/10;
+        rev=rev*10+result;
+        if(n==0){break;
+        }
+     }
+
+     if(rev==a){
+      cout<<"This number is palindrome"<<endl;}
+      else
+      {
+          cout<<"This number is not palindrome";
+      }
+}
+      return 0;
+
+
+}
